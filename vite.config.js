145b0d8af50b265ce315f3bc848417f124ad790e
Yuri Ochoa <ypochoag@gmail.com>
2024-12-06 21:27:15 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ctd-esp-fe3-final/',
+  base: 'https://ypochoag.github.io/ctd-esp-fe3-final',
 })
